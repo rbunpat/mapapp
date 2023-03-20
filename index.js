a2d63776ae51b23043e4810b58f5d303d5c56f8d
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-03-20 21:33:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,98 @@
-const express = require('express');
-fs = require('fs');
-url = require('url');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const getLastLine = require('./fileTools.js').getLastLine
-const fileName = 'bruh.txt';
-const minLineLength = 1
+const express = require("express");
+const bodyParser = require("body-parser");
+const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
-const port = 80;
+const port = 8080;
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+const githubLink = "<a href='https://github.com/rbunpat/mapapp'>https://github.com/rbunpat/mapapp</a>"
 
-app.get('/', function(req, res) {
-    res.send("yay it works");
-    });
-
-app.post('/savePos', function(request, respond) {
-    var body = '';
-    filePath = 'bruh.txt';
-    request.on('data', function(data) {
-        body += data;
-    });
-
-    request.on('end', function (){
-        fs.appendFile(filePath, body, function() {
-            respond.end();
-        });
-    });
+const db = new sqlite3.Database("coordinates.db", (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log("Connected to the tracker database.");
 });
 
-app.get('/getPos', function(request, respond) {
-    getLastLine(fileName, minLineLength).then((lastLine) => {
-        respond.send(lastLine);
-    });
+db.run(`CREATE TABLE IF NOT EXISTS positions (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  latitude REAL NOT NULL,
+  longitude REAL NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)`);
+
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+  res.send(
+    "Tracker API Server, For documentation see " + githubLink
+  );
 });
 
+app.get("/data", function (req, res) {
+  res.send(
+    "404 Not Found, See " + githubLink
+  );
+});
+
+app.post("/data/save", function (req, res) {
+  const { latitude, longitude } = req.body;
+
+  if (!latitude || !longitude) {
+    return res.status(400).send("Latitude and longitude are required.");
+  }
+
+  const query = `INSERT INTO positions (latitude, longitude) VALUES (${latitude}, ${longitude})`;
+
+  db.run(query, function (err) {
+    if (err) {
+      return res.status(500).send(`{
+        "status": "400 Bad Request"
+    }`);
+    }
+    res.send(`{
+    "status": "200 OK"
+}`);
+  });
+});
+
+app.get("/data/json", function (req, res) {
+  const query = `SELECT latitude, longitude FROM positions ORDER BY created_at DESC LIMIT 1`;
+
+  db.get(query, function (err, row) {
+    if (err) {
+      return res.status(500).send(`{
+        "status": "400 Bad Request"
+    }`);
+    }
+    if (!row) {
+      return res.status(404).send(`{
+        "status": "No Positions in Database"
+    }`);
+    }
+    res.send(`{
+      "latitude": "${row.latitude}",
+      "longitude": "${row.longitude}"
+  }`);
+  });
+});
+
+app.get("/data/plain", function (req, res) {
+  const query = `SELECT latitude, longitude FROM positions ORDER BY created_at DESC LIMIT 1`;
+
+  db.get(query, function (err, row) {
+    if (err) {
+      return res.status(500).send("400 Bad Request, See " + githubLink);
+    }
+    if (!row) {
+      return res.status(404).send("No positions found.");
+    }
+    res.send(`${row.latitude}, ${row.longitude}`);
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
 
 module.exports = app;
